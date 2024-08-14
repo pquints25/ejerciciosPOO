6972dc47cbb5778b202bbc401ad326c7bb229830
paolo <paoloquintiliani91@gmail.com>
2024-08-14 11:28:31 -0400

    aaa

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 #ejercicios en clases
 ##aaaa
+
+aumentar y descontar stock del ejercicio uno, deben estar dentro de la clase, si no tirara error
--- assets/js/mainEjercicioUno.js
+++ assets/js/mainEjercicioUno.js
@@ -1,10 +1,13 @@
 import Producto from "./ejercicioUno.js";
 
-let productoUno = new Producto ('1', 'telefono', 150000, '30');
-let productoDos = new Producto ('2', 'reloj', 90000, '20');
-let productoTres = new Producto ('3', 'audifonos', 100000, '40');
-let productoCuatro = new Producto ('4', 'carcasa', 10000, '100');
+let productoUno = new Producto ('1', 'telefono', 150000, 30);
+let productoDos = new Producto ('2', 'reloj', 90000, 20);
+let productoTres = new Producto ('3', 'audifonos', 100000, 40);
+let productoCuatro = new Producto ('4', 'carcasa', 10000, 100);
 
+productoUno.descontarStock(4);
+productoUno.descontarStock(2)
+productoUno.aumentarStock(6);
 
 console.log(productoUno, productoDos, productoTres, productoCuatro);
 
